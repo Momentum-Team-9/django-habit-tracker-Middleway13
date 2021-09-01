b618137e19336d70eb0096ffa72dca7de6229f32
Middleway13 <middlewayonlinemedia@gmail.com>
2021-09-01 10:41:07 -0400

    admin.py modified

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import User, Habit, DailyResult
 
-admin.site.register(User)
-admin.site.register(Habit)
-admin.site.register(DailyResult)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(DailyResult)
+class DailyResultAdmin(admin.ModelAdmin):
+    pass
