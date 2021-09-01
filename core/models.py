67a29d3d3404c541df081523c2ec5ff6378cbbb5
Middleway13 <middlewayonlinemedia@gmail.com>
2021-08-31 22:37:21 -0400

    models.py created

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import date
+from django.db.models.constraints import UniqueConstraint
 
 class User(AbstractUser):
     def __repr__(self):
@@ -7,3 +9,25 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Habit(models.Model):
+    goal = models.CharField(max_length=100)
+    target = models.TextField()
+    created_date = models.DateField(default=date.today)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='habits')
+    
+    def __str__(self):
+        return f"{self.goal}"
+
+class DailyResults (models.Model):
+    habit= models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='results', null=True)
+    results = models.PositiveIntegerField()
+    date = models.DateField(default=date.today)
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['habit', 'date'], name='daily_record')
+        ]
+        
+        def __str__(self):
+            return f"{self.date} {self.habit}"
